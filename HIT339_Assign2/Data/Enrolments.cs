013fcf3d7c0a3cb64139d0b0f041b4cf47fe4128
ZephyrGitCode <zephyr.dobson@outlook.com>
2020-09-28 21:33:45 +0930

    Added Enrolments FK to Users and schedules, Schedules FK to Enrolments  and finished DB design (mostly). Also Scaffolded.

---
+++ HIT339_Assign2/Data/Enrolments.cs
@@ -0,0 +1,23 @@
+﻿using HIT339_Assign2.Areas.Identity.Data;
+using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HIT339_Assign2.Data
+{
+    public class Enrolment
+    {
+        public int Id { get; set; }
+
+        public int ScheduleId { get; set; }
+
+        public string UserId { get; set; }
+
+        public virtual Schedule Schedule { get; set; }
+        public virtual ApplicationUser User { get; set; }
+    }
+}
