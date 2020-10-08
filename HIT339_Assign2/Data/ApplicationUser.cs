b26cd352492087e47dfcf85b47f3c82d6ccbd72b
ZephyrGitCode <zephyr.dobson@outlook.com>
2020-10-08 17:41:09 +0930

    Latest with Bai and Theo's changes
    Signed-off-by: ZephyrGitCode <zephyr.dobson@outlook.com>

--- HIT339_Assign2/Data/ApplicationUser.cs
+++ HIT339_Assign2/Data/ApplicationUser.cs
@@ -11,27 +11,21 @@ namespace HIT339_Assign2.Areas.Identity.Data
     // Add profile data for ApplicationUser users by adding properties to the Assign1_Salesboard_ZephyrUser class
     public class ApplicationUser : IdentityUser
     {
-        [Display(Name = "First Name")]
         [StringLength(25), PersonalData]
         public string Fname { get; set; }
 
-        [Display(Name = "Last Name")]
         [StringLength(50), PersonalData]
         public string Lname { get; set; }
 
-        [Display(Name = "Age")]
-        [Range(15,120), PersonalData]
+        [Range(16,120), PersonalData]
         public int Age { get; set; }
 
-        [Display(Name = "Gender")]
         [PersonalData]
         public string Gender { get; set; }
 
-        [Display(Name = "Biography")]
         [PersonalData]
         public string Biography { get; set; }
 
-        [Display(Name = "Enrolments")]
         public virtual ICollection<Enrolment> Enrolments { get; set; }
     }
 }
